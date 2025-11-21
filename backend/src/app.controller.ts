a0b5bf62183a02f69ee551e1f82ce8c077e8503f
Rhenan Dias <diasrhenan@gmail.com>
2025-11-21 07:59:14 -0300

    Adicionando configurações iniciais do nestjs e mais arquivos de utilidades

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller()
+@ApiTags('API')
 export class AppController {
   constructor() {}
 
+  @ApiOperation({
+    summary: 'Raiz da API',
+  })
   @Get()
   execute() {
     return {
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '../.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
