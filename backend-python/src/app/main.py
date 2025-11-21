d41e115f7db613dcc7008669b8815a32ae3d490d
Rhenan Dias <diasrhenan@gmail.com>
2025-11-21 17:37:04 -0300

    Setup inicial do projeto em Python

--- backend-python/src/app/main.py
+++ backend-python/src/app/main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from fastapi import FastAPI
 
 from app.api.v1 import user
@@ -9,6 +11,17 @@ setup_logging()
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title=config.app_name)
+
+
+# Health check route
+@app.get("/")
+async def health_check():
+    return {
+        "message": "Estou vivo, e respondendo em Python!",
+        "service": config.app_name,
+        "timestamp": datetime.now().isoformat(),
+        "version": "1.0.0"
+    }
 
 
 # Register routes
