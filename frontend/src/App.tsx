73e40ce24f1f3bd8b480b573f11a54282222318f
Rhenan Dias <diasrhenan@gmail.com>
2025-11-21 07:04:00 -0300

    Configurando Shadcnui no boilerplate de frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <div>
       <h1 className="text-red-500">Hello World</h1>
+      <Button>Tudo bem?</Button>
     </div>
   );
 }
